b3715be3d35037b5182bf72eb91b5fc5394cb22a
Cole Crawford <cole_crawford@harvard.edu>
2021-06-14 18:14:26 -0400

    Update show.php
    - More display logic

--- simple-pages/page/show.php
+++ simple-pages/page/show.php
@@ -7,10 +7,10 @@
 /** Set up variables based on page */
 $curPageName = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);  
 $requestUri = $_SERVER['REQUEST_URI'];
-$bannerUri = '';
+$bannerUri = '/themes/eileen-southern-theme/images/';
 switch($requestUri){
     case '/about':
-        $bannerUri = 'images/page_banner_12.png';
+        $bannerUri .= 'page_banner_12.png';
         break;
 }
 
@@ -18,14 +18,26 @@ switch($requestUri){
 
 
 ?>
-<h3 style="color:white"><?php echo $curPageName . ' ' . $_SERVER['REQUEST_URI']?></h3>
-<p id="simple-pages-breadcrumbs" class="navigation secondary-nav"><?php echo simple_pages_display_breadcrumbs(); ?></p>
-<h1><?php echo metadata('simple_pages_page', 'title'); ?></h1>
+<!-- <h3 style="color:white"><?php echo $curPageName . ' ' . $_SERVER['REQUEST_URI']?></h3> -->
+<!-- <p id="simple-pages-breadcrumbs" class="navigation secondary-nav"><?php echo simple_pages_display_breadcrumbs(); ?></p> -->
+<main>
+    <div class="banners">
+        <img src="<?php echo $bannerUri; ?>">
+        <p class="caption">Image caption ...</p>
+    </div>
+    <div class="container-narrow lengthen-page">
+    <?php
+        $text = metadata('simple_pages_page', 'text', array('no_escape' => true));
+        echo $this->shortcodes($text);
+    ?>
+    </div>
+</main>
+<!-- <h1><?php echo metadata('simple_pages_page', 'title'); ?></h1>
 <div id="primary">
     <?php
     $text = metadata('simple_pages_page', 'text', array('no_escape' => true));
     echo $this->shortcodes($text);
     ?>
-</div>
+</div> -->
 
 <?php echo foot(); ?>
