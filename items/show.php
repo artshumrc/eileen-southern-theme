4d188b610b00e35515049866a322093f0cd492c9
Cole Crawford <cole_crawford@harvard.edu>
2021-06-15 23:04:32 -0400

    Display metadata correctly on single item pages

--- items/show.php
+++ items/show.php
@@ -1,57 +1,50 @@
-<?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'item show')); ?>
+<?php 
+    echo head(array('title' => 'Gallery','bodyclass' => 'item show'));
+    $file=null;
+    if($item->getFile(0)){
+        $file = get_record_by_id('File', $item->getFile(0)->id);
+    }
 
-<h1><?php echo metadata('item','rich_title', array('no_escape' => true)); ?></h1>
+    all_element_texts('item', array(
+        'show_element_set_headings' => false,
+        'partial' => 'common/southern-metadata.php',
+    ))
+?>
+<main>
+    <div class="container-wide">
+        <div class="flex-column">
+          <div class="gallery-image-single">
+            <img data-aos="fade-up" src="<?php if($file){ echo metadata($file, 'fullsize_uri');}?>">
+          </div>
+          <div class="gallery-description-single">
+            <h6>Object <?php echo $item->id . '/' . total_records('Item');?> </h6>
+            <h4><?php echo metadata('item', 'display_title');?></h4>
+            <h5 class="gallery-category"><?php echo metadata('item', array('Dublin Core', 'Format')); ?></h5>
+            <?php if(metadata('item', array('Dublin Core', 'Description'))): ?>
+                <div class="gallery-description">
+                    <?php echo metadata('item', array('Dublin Core', 'Description')); ?>
+                </div>
+            <?php endif; ?>
+            <div class="gallery-data-container">
+                <?php
+                    echo all_element_texts('item', array(
+                        'show_element_set_headings' => false,
+                        'partial' => 'common/southern-metadata.php',
+                    ));
+                ?>
+            </div>
+          </div>
 
-    <?php if (get_theme_option('Item FileGallery') == 0 && metadata('item', 'has files')): ?>
-    <div class="element-text"><?php echo files_for_item(array('imageSize' => 'fullsize')); ?></div>
-    <?php endif; ?>
+          <div class="sub-nav">
+            <h6><?php echo link_to_previous_item_show('← Previous'); ?></h6>
+            <a href="/items/browse"><img src="<?php echo img('thumb-icon.svg'); ?>"></a>
+            <h6><?php echo link_to_next_item_show('Next →'); ?></h6>
+          </div>
+        </div>
+      </div>
 
-
-<?php echo all_element_texts('item'); ?>
-
-<!-- The following returns all of the files associated with an item. -->
-<?php if (metadata('item', 'has files') && (get_theme_option('Item FileGallery') == 1)): ?>
-<div id="itemfiles" class="element">
-    <h3><?php echo __('Files'); ?></h3>
-    <div class="element-text"><?php echo item_image_gallery(); ?></div>
-</div>
-<?php endif; ?>
-
-<!-- If the item belongs to a collection, the following creates a link to that collection. -->
-<?php if (metadata('item', 'Collection Name')): ?>
-<div id="collection" class="element">
-    <h3><?php echo __('Collection'); ?></h3>
-    <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
-</div>
-<?php endif; ?>
-
-<!-- The following prints a list of all tags associated with the item -->
-<?php if (metadata('item', 'has tags')): ?>
-<div id="item-tags" class="element">
-    <h3><?php echo __('Tags'); ?></h3>
-    <div class="element-text"><?php echo tag_string('item'); ?></div>
-</div>
-<?php endif;?>
-
-<!-- The following prints a citation for this item. -->
-<div id="item-citation" class="element">
-    <h3><?php echo __('Citation'); ?></h3>
-    <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
-</div>
-
-<!-- Available output formats --> 
-<div id="item-output-formats" class="element">
-    <h2><?php echo __('Output Formats'); ?></h2>
-    <div class="element-text"><?php echo output_format_list(); ?></div>
-</div>
+</main>
 
 <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
-
-<nav>
-<ul class="item-pagination navigation">
-    <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-    <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-</ul>
-</nav>
 
 <?php echo foot(); ?>
