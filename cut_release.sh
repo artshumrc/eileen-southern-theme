4ff55844b4c1effa9afd158e8070ffd61fda5d4e
Cole Crawford <cole_crawford@harvard.edu>
2021-10-22 12:03:14 -0400

    Exit release script if release not named

--- cut_release.sh
+++ cut_release.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Cuts a Github release; this is necessary because the Git submodule wouldn't get included in the Git released .zip
 # Arg: the release name
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Supply a name for the release as a CLI arg'
+    exit 0
+fi
+
 cd ..
 cp -r eileen-southern-theme/ ~/Documents/southern_releases/eileen-southern-theme-$1
 cd ~/Documents/southern_releases/eileen-southern-theme-$1
