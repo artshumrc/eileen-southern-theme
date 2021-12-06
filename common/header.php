464756327e0b1ebf4bc99c0c1de3fcce4dfdccb1
Cole Crawford <cole_crawford@harvard.edu>
2021-12-06 14:28:06 -0500

    Add Google Analytics

--- common/header.php
+++ common/header.php
@@ -71,6 +71,15 @@
     queue_js_file('default');
     echo head_js();
     ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3H9NF37N0L"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-3H9NF37N0L');
+    </script>
 </head>
 
 <?php
